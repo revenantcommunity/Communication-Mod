400f406fcd4b729684ba6503b405ac7f9bd7105d
Rhenar <contact@studioblackraven.fr>
2016-05-30 21:23:21 +0200

    Ajout du init.lua
    Ajout du chargement SERVER et CLIENT
    Ajout du texte de chargement

---
+++ lua/autorun/init.lua
@@ -0,0 +1,37 @@
+-----------------------
+-- Communication Mod --
+-----------------------
+-- Auteur : Happy	 --
+-- Auteur : Rhenar   --
+-----------------------
+-- Version : 0.1     --
+-----------------------
+
+-- CHARGEMENT SERVER FILE --
+
+if SERVER then
+
+
+
+end
+
+-- CHARGEMENT CLIENT FILE --
+
+if CLIENT then
+
+
+
+end
+
+-- TEXT CHARGEMENT --
+
+print("\n")
+MsgC(Color(0,255,0), "-------------------------------------------------------------------------------\n")
+print("\n")
+MsgC(Color(255, 0, 0), " > ") MsgC(Color(255,255,255), "Communication Mod\n")
+print("\n")
+MsgC(Color(255, 0, 0), " > ") MsgC(Color(255,255,255), "Version: 0.1\n")
+MsgC(Color(255, 0, 0), " > ") MsgC(Color(255,255,255), "Auteur: Happy et Rhenar\n")
+print("\n")
+MsgC(Color(0,255,0), "-------------------------------------------------------------------------------\n")
+print("\n")
